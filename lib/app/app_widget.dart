7d77353f3b7f5b108a1786b988e5d716c3aabd09
Gabriel Santos <gabrielt.santos.gs@gmail.com>
2020-12-09 18:34:18 -0300

    Ajustes inciais

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:get/get.dart';
+
+import 'core/theme_cuidapet.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      navigatorKey: Modular.navigatorKey,
+      title: 'Flutter Slidy',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeCuidapet.theme(),
+      initialRoute: '/',
+      onGenerateRoute: Modular.generateRoute,
+    );
+  }
+}
